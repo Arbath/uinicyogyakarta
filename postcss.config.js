a66d1227d6f81a9fa2ad5d8c31b9cc153d4145d8
Arbath Abdurrahman <arbath21@gmail.com>
2024-09-23 20:26:08 +0700

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+};
+
+// develop by : https://github.com/ArbathAbdurrahman
---
+++ static/js/script.js
@@ -0,0 +1,37 @@
+// develop by : https://github.com/ArbathAbdurrahman
+document.addEventListener("DOMContentLoaded", function() {
+    const menu = document.getElementById('menu');
+    const navbar = document.getElementById('navbar');
+    const menuButton = document.getElementById('menu-button');
+
+    // Toggle menu saat tombol hamburger diklik
+    menuButton.addEventListener('click', function() {
+        menu.classList.toggle('hidden');
+    });
+
+    // Menyembunyikan menu saat tautan diklik
+    const menuLinks = document.querySelectorAll('#menu a');
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            if (!menu.classList.contains('hidden')) {
+                menu.classList.add('hidden'); // Sembunyikan menu
+            }
+        });
+    });
+
+    // Event listener untuk scroll
+    let lastScrollTop = 0;
+    window.addEventListener('scroll', function() {
+        const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (currentScroll > 50) {
+            if (currentScroll > lastScrollTop) {
+                navbar.classList.add('hidden'); // Scroll ke bawah
+            } else {
+                navbar.classList.remove('hidden'); // Scroll ke atas
+            }
+        }
+
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Menyimpan posisi scroll terakhir
+    });
+});
